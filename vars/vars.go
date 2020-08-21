7395d1c3b0275328f65cab527d05c9b29bbcb458
Onism <onism68@gmail.com>
2020-08-21 14:10:27 +0800

    vars单独目录，避免重复引用

---
+++ vars/vars.go
@@ -0,0 +1,27 @@
+package vars
+
+import "time"
+
+var (
+	MasterIp        string
+	NodeIps         []string
+	PkgPath         = "/root/workSpaceTmp/"
+	PkgName         = ""
+	WorkSpace       = "/root/workSpaceTmp/"
+	ErrorExitOSCase = -1 // 错误直接退出类型
+
+)
+
+var SSHConfig struct {
+	User           string
+	Password       string
+	PrivateKeyPath string
+}
+
+type SSH struct {
+	User       string
+	Password   string
+	PkFile     string
+	PkPassword string
+	Timeout    *time.Duration
+}
