a0900c13d4c1d37bdc4983cc13ad085bd83df7b3
Onism <onism68@gmail.com>
2020-08-23 20:17:02 +0800

    安装命令部分步骤

--- install/install.go
+++ install/install.go
@@ -3,24 +3,131 @@ package install
 import (
 	"fmt"
 	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/os/glog"
+	"github.com/gogf/gf/text/gregex"
 	"github.com/onism68/helmOnap/utils"
+	"github.com/onism68/helmOnap/vars"
+	"os"
+	"time"
 )
 
 func RunInstall() {
-	port := g.Cfg().GetString("port")
 
-	nodes := utils.ParseIPs(NodeIps)
-	pkgUrl := fmt.Sprintf("http://%s:%s/%s", MasterIp, port, PkgName)
-	runInNode(nodes, pkgUrl)
+	// 创建工作目录
+	//utils.CmdsInLocal("mkdir", []string{vars.WorkSpace}, nil, nil)
+	//// master解压包
+	//runInMaster("touch", []string{"test.test"})
+	//runInMaster("tar", []string{"-zxvf", vars.PkgName, "-C", vars.WorkSpace})
+	//// 安装helm
+	//runInMaster("cp", []string{"./helm/helm", "/usr/local/bin/"})
+	//// 配置权限
+	//runInMaster("chmod", []string{"+x", "/usr/local/bin/helm"})
+	//
+	//runInMaster("kubectl", []string{"create", "serviceaccount --namespace=kube-system tiller"})
+	//runInMaster("kubectl", []string{"create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller"})
+	////helm init
+	//runInMaster("helm", []string{"init", "--upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.16.6 --stable-repo-url http://mirror.azure.cn/kubernetes/charts/ --service-account=tiller"})
+	//runInMaster("helm", []string{"repo remove stable"})
+
+	//runInMaster("cp", []string{"-r", "./helm/plugins", "~/.helm/"})
+	//s := utils.CmdToString("helm", fmt.Sprintf("serve %s", "&"))
+	//glog.Println(s)
+	//runInMaster("helm", []string{fmt.Sprintf("serve %s", "&")})
+
+	//return
+
+	//通过ssh链接本地
+	sshMaster := utils.SSH{
+		User:     "root",
+		Password: "0222",
+		//PkFile:     "/root/.ssh/id_rsa",
+		PkPassword: "",
+		Timeout:    nil,
+	}
+	//创建工作目录
+	sshMaster.CmdInMaster(MkdirCom(vars.WorkSpace))
+
+	//sshMaster.CmdInMaster(TarX(vars.PkgPath+vars.PkgName, vars.WorkSpace))
+	//sshMaster.CmdInMaster(TarX(vars.WorkSpace + "onap-f-6.0.0.tgz", vars.WorkSpace))
+
+	sshMaster.CmdInMaster(Cpr(vars.WorkSpace+"/helm/helm", "/usr/local/bin/"))
+	sshMaster.CmdInMaster(Chmod("+x", "/usr/local/bin/helm"))
+
+	//sshMaster.CmdInMaster("kubectl create serviceaccount --namespace=kube-system tiller")
+	//sshMaster.CmdInMaster("kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin " +
+	//	"--serviceaccount=kube-system:tiller")
+	//sshMaster.CmdInMaster("helm init --upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.16.6 " +
+	//	"--stable-repo-url http://mirror.azure.cn/kubernetes/charts/ --service-account=tiller")
+	//
+
+	// 检查tiller服务是否就绪
+	tillerInfo := sshMaster.CmdInServer(vars.MasterIp, "kubectl get pods -n kube-system | grep tiller")
+	readyList, err := gregex.MatchString(`(\d+)\/(\d+)`, string(tillerInfo))
+	if err != nil {
+		panic(err)
+	}
+	forTimes := 0
+	for readyList[1] != readyList[2] {
+		glog.Info("tiller 服务未就绪")
+		tillerInfo := sshMaster.CmdInServer(vars.MasterIp, "kubectl get pods -n kube-system | grep tiller")
+		readyList, err = gregex.MatchString(`(\d+)\/(\d+)`, string(tillerInfo))
+		if err != nil {
+			panic(err)
+		}
+		time.Sleep(time.Second)
+		forTimes++
+		if forTimes > 60 {
+			glog.Error("tiller 服务持续未就绪，exit")
+			os.Exit(vars.ErrorExitOSCase)
+		}
+	}
+	glog.Info("tiller 服务已就绪，进行后续操作")
+	//sshMaster.CmdInMaster("helm repo remove stable")
+	sshMaster.CmdInMaster(Cpr(vars.WorkSpace+"helm/plugins", "~/.helm/"))
+	//sshMaster.CmdInMaster("helm create namespace onap")
+	// 创建namespace
+	sshMaster.CmdInMaster("kubectl create namespace onap")
+
+	sshMaster.CmdInMaster(fmt.Sprintf("docker load -i %s", vars.WorkSpace+"docker/docker.tar || true"))
+	//sshMaster.CmdInMaster(Chmod("+x", vars.WorkSpace + "docker/docker.sh"))
+	//sshMaster.CmdInMaster(fmt.Sprintf("/bin/sh %s", vars.WorkSpace+"docker/docker.sh"))
+	// todo 暂时无法后台运行，重新考虑
+	//sshMaster.CmdInServer(vars.MasterIp, "helm serve >> out.txt 2>&1 &")
+
+	//sshMaster.CmdInMaster(HelmInstall(VFC + " --set global.masterPassword=onap"))
+	//return
+
+	// 下载包，配置本地文件服务器地址
+	port := g.Cfg().GetString("port")
+	nodes := utils.ParseIPs(vars.NodeIps)
+	pkgUrl := fmt.Sprintf("http://%s:%s/%s", vars.MasterIp, port, vars.PkgName)
+	// 创建目录
+	runInNode(nodes, MkdirCom(vars.WorkSpace))
+	// 下载包
+	runInNode(nodes, WgetCom(pkgUrl, vars.PkgName))
+	// 解压包
+	runInNode(nodes, TarX(vars.PkgName, vars.WorkSpace))
+	runInNode(nodes, fmt.Sprintf("docker load -i %s", vars.WorkSpace+"docker/docker.tar"))
+
 }
 
-func runInNode(nodes []string, pkgUrl string) {
-	for _, nodeIp := range nodes {
-		args := []string{nodeIp}
-		extraName := []string{"ping"}
-		//extraArgs := [][]string{[]string{pkgUrl}}
-		extraArgs := [][]string{[]string{"-c", "10", "192.168.2.1"}, []string{"pwd"}}
-		utils.RunCmd("ssh", args, extraName, extraArgs)
-	}
+func runInMaster(name string, args []string) {
+	utils.CmdInLocal(name, args)
+}
 
+func runInNode(nodes []string, arg string) {
+	for _, nodeIp := range nodes {
+		ssh := utils.SSH{
+			User:       vars.SSHConfig.User,
+			Password:   vars.SSHConfig.Password,
+			PkFile:     vars.SSHConfig.PrivateKeyPath,
+			PkPassword: "",
+			Timeout:    nil,
+		}
+		//_ = ssh.CmdInServer(nodeIp, MkdirCom(vars.WorkSpace))
+		err := ssh.CmdAsync(nodeIp, arg)
+		if err != nil {
+			glog.Error(err.Error())
+		}
+	}
 }
--- install/install_test.go
+++ install/install_test.go
@@ -1,9 +1,53 @@
 package install
 
-import "testing"
+import (
+	"fmt"
+	"github.com/gogf/gf/os/glog"
+	"github.com/gogf/gf/text/gregex"
+	"github.com/onism68/helmOnap/utils"
+	"github.com/onism68/helmOnap/vars"
+	"testing"
+)
 
 func TestRunInstall(t *testing.T) {
-	MasterIp = "127.0.0.1"
-	NodeIps = []string{"-l", "onism", "-L", "192.168.2.249"}
+	vars.MasterIp = "127.0.0.1"
+	vars.NodeIps = []string{"-l", "onism", "-L", "192.168.2.249"}
 	RunInstall()
+}
+
+func Test_runInNode(t *testing.T) {
+	vars.SSHConfig.User = "root"
+	vars.SSHConfig.Password = "0222"
+	vars.MasterIp = "172.21.80.101"
+	sshMaster := utils.SSH{
+		User:     "root",
+		Password: "0222",
+		//PkFile:     "/root/.ssh/id_rsa",
+		PkPassword: "",
+		Timeout:    nil,
+	}
+
+	//runInNode([]string{"172.21.80.101"}, CdCom(vars.WorkSpace))
+
+	//runInNode([]string{"172.21.80.101"}, WgetCom("172.21.80.1:8199/main.exe"))
+
+	// 检查tiller服务是否就绪
+	tmp := sshMaster.CmdInServer(vars.MasterIp, "kubectl get pods -n kube-system | grep tiller")
+	fmt.Println(tmp)
+	readyList, err := gregex.MatchString(`(\d+)\/(\d+)`, string(tmp))
+	if err != nil {
+		panic(err)
+	}
+	readyList[1] = "1"
+	readyList[2] = "2"
+	for readyList[1] != readyList[2] {
+		glog.Info("tiller 服务未就绪")
+		tillerInfo := sshMaster.CmdInServer(vars.MasterIp, "kubectl get pods -n kube-system | grep tiller")
+		readyList, err = gregex.MatchString(`(\d+)\/(\d+)`, string(tillerInfo))
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("yijiuxu")
+	fmt.Println(readyList)
 }
