9430b31a8b9aba612db4e07a60f03d9ef826aebd
Onism <onism68@gmail.com>
2020-08-23 20:14:09 +0800

    参数调整

--- cmd/init.go
+++ cmd/init.go
@@ -4,21 +4,22 @@ import (
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/os/glog"
 	"github.com/onism68/helmOnap/install"
+	"github.com/onism68/helmOnap/vars"
 	"github.com/spf13/cobra"
 	"path"
 )
 
 var initCmd = &cobra.Command{
-	Use: "init",
+	Use:   "init",
 	Short: "init",
-	Long: "init",
+	Long:  "init",
 	Run: func(cmd *cobra.Command, args []string) {
-	//
+		//
 	},
 	PostRun: func(cmd *cobra.Command, args []string) {
 		glog.Info("写入临时路径")
-		install.PkgPath, install.PkgName = path.Split(install.PkgPath)
-		err := g.Cfg().Set("Pkg.PkgPath", install.PkgPath)
+		vars.PkgPath, vars.PkgName = path.Split(vars.PkgPath)
+		err := g.Cfg().Set("Pkg.PkgPath", vars.PkgPath)
 		if err != nil {
 			glog.Error("写入临时路径出错：%s", err.Error())
 		}
@@ -30,19 +31,20 @@ var initCmd = &cobra.Command{
 	},
 }
 
-
 func init() {
 	rootCmd.AddCommand(initCmd)
 
 	// todo 读取配置文件，更新
-	initCmd.Flags().StringVar(&install.SSHConfig.User, "user", "root", "servers user name for ssh")
-	initCmd.Flags().StringVar(&install.SSHConfig.Password, "password", "", "servers user password for ssh")
-	initCmd.Flags().StringVar(&install.SSHConfig.PrivateKeyPath, "PrivateKeyPath", "/root/.ssh/id_rsa", "private key for ssh")
-	
-	initCmd.Flags().StringVar(&install.PkgPath, "pkgPath", "/root/helmOnap/xxx.tar.gz", "path of pkg ex. /root/helmOnap/test.tar.gz")
-	initCmd.MarkFlagRequired("pkgPath")
-	initCmd.Flags().StringVar(&install.MasterIp, "MasterIp", "192.168.0.2", "kubernetes multi-masters ex. 192.168.0.2")
-	initCmd.Flags().StringSliceVar(&install.NodeIps, "NodeIps", []string{}, "kubernetes multi-nodes ex. 192.168.0.5, 192.168.0.6")
+	initCmd.Flags().StringVar(&vars.SSHConfig.User, "user", "root", "servers user name for ssh")
+	initCmd.Flags().StringVar(&vars.SSHConfig.Password, "password", "0222", "servers user password for ssh")
+	initCmd.Flags().StringVar(&vars.SSHConfig.PrivateKeyPath, "PrivateKeyPath", "/root/.ssh/id_rsa", "private key for ssh")
 
+	initCmd.Flags().StringVar(&vars.PkgPath, "pkgPath", "/root/helmOnap/xxx.tar.gz", "path of pkg ex. /root/helmOnap/test.tar.gz")
+	initCmd.Flags().StringVar(&vars.MasterIp, "masterIp", "192.168.0.2", "kubernetes multi-masters ex. 192.168.0.2")
+	initCmd.Flags().StringSliceVar(&vars.NodeIps, "nodeIps", []string{}, "kubernetes multi-nodes ex. 192.168.0.5, 192.168.0.6")
+
+	initCmd.MarkFlagRequired("masterIp")
+	initCmd.MarkFlagRequired("nodeIps")
+	initCmd.MarkFlagRequired("pkgPath")
 
 }
