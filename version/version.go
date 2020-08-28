fd38d6ed5b3eb401af87833ad3672d9829baef05
onism68 <onism68@gmail.com>
2020-08-28 23:47:04 +0800

    buildTime固定，暂未想到解决办法

--- version/version.go
+++ version/version.go
@@ -1,16 +1,13 @@
 package version
 
 import (
-"fmt"
-	"github.com/gogf/gf/os/gtime"
+	"fmt"
 	"runtime"
 )
 
 var (
-	Version   = "0.0.1"
-	Build     = ""
-	BuildTime = gtime.Datetime()
+	Version    = "0.0.1"
+	Build      = "linux_amd64"
+	BuildTime  = "2020-08-28 19:00"
 	VersionStr = fmt.Sprintf(" version: %v \n build: %v %v \n Build Time: %v", Version, Build, runtime.Version(), BuildTime)
 )
-
-
